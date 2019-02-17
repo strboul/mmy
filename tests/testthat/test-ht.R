791485dfe219ce922b915c22af82fa0c869acaee
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-02-17 11:25:26 +0100

    Cosmetic changes

--- tests/testthat/test-ht.R
+++ tests/testthat/test-ht.R
@@ -7,29 +7,29 @@ skip(NULL)
 iris[["charcol"]] <- rep_len(letters, nrow(iris))
 
 test_that("be sure the iris is modified", {
-  
+
   expect_s3_class(iris, "data.frame")
-  
+
   expect_equal(dim(iris), c(150L, 6L))
-  
+
   expect_is(iris[["Sepal.Length"]], "numeric")
   expect_type(iris[["Sepal.Length"]], "double")
-  
+
   expect_is(iris[["Sepal.Width"]], "numeric")
   expect_type(iris[["Sepal.Width"]], "double")
-  
+
   expect_is(iris[["Petal.Length"]], "numeric")
   expect_type(iris[["Petal.Length"]], "double")
-  
+
   expect_is(iris[["Petal.Width"]], "numeric")
   expect_type(iris[["Petal.Width"]], "double")
-  
+
   expect_is(iris[["Species"]], "factor")
   expect_type(iris[["Species"]], "integer")
-  
+
   expect_is(iris[["charcol"]], "character")
   expect_type(iris[["charcol"]], "character")
-  
+
 })
 
 test_that("see head and tail of a data frame", {
@@ -53,11 +53,11 @@ test_that("see head and tail of a data frame", {
   # TODO
   # if n is set a little less, equal, and more than the nrow of df:
   ht(iris, n = (nrow(iris) - 1L))
-  
+
   ht(iris, n = nrow(iris))
-  
+
   ht(iris, n = (nrow(iris) + 1L))
-  
+
 	# different input types & lengths:
 	expect_error(ht("A"))
 	expect_error(ht(c("a", "b", "c")))
@@ -66,7 +66,7 @@ test_that("see head and tail of a data frame", {
 	expect_error(ht(list(x = seq(10))))
 
 	expect_error(ht(c(iris, mtcars)))
-	
+
 	expect_error(ht(iris, "A"))
 	expect_error(ht(iris, c("A", "B")))
 
