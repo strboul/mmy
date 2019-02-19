bb990144d8d22b2a4ba62b2be06091363af4c38b
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-02-19 22:04:41 +0100

    Add header file mmy.h

--- src/ht.c
+++ src/ht.c
@@ -1,20 +1,12 @@
 
-/*
- * Safer to use defined macros if you work with R version greater than 3.3. See:
- * https://github.com/wch/r-source/blob/f5e9878c4fe7453124cef5e56649cef62ef61d77/src/include/Rinternals.h#L1362
- *
-*/
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
+#include "mmy.h"
 
 SEXP _ht (SEXP df, SEXP n) {
 
-	// TODO arg length Rf_error("input length must be 1");
 	if (!Rf_isFrame(df)) Rf_error("input must be a data.frame");
 
-	// TODO arg length Rf_error("input length must be 1");
-	if (!Rf_isInteger(n) || Rf_isReal(n)) Rf_error("n isn't numeric");
+	if (!(Rf_xlength(n) == 1)) Rf_error("input length must be 1");
+	if (!(Rf_isInteger(n) || Rf_isReal(n))) Rf_error("n isn't numeric");
 
 	int nn = Rf_asInteger(n);
 	// Rf_xlength is up to 64-bit unlike Rf_length which is 32-bit.
