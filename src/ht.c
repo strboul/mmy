c98b9b5924b4fdefdf84e013db0649a1a310efae
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-03-23 18:12:37 +0100

    Complete max_cols

--- src/ht.c
+++ src/ht.c
@@ -2,16 +2,17 @@
 // Rf_xlength is for up to 64-bit unlike Rf_length which is 32-bit.
 #include "mmy.h"
 
-void prt_hspace(int *len) { Rprintf("%*s", *len, ""); }
+void prt_hspace(int len) { Rprintf("%*s", len, ""); }
 
-void prt_colnames(SEXP df, int *ncol, int *nrowlen) {
+void prt_colnames(SEXP df, int ncol, int nrow) {
 	SEXP names = PROTECT(Rf_getAttrib(df, R_NamesSymbol));
+	int nrowlen = nint(&nrow);
 	prt_hspace(nrowlen);
-	for (R_xlen_t i = 0; i < *ncol; i++) {
+	for (R_xlen_t i = 0; i < ncol; i++) {
 		Rprintf("%s ", CHAR(STRING_ELT(names, i)));
 	}
-	UNPROTECT(1);
 	Rprintf("\n");
+	UNPROTECT(1);
 }
 
 void prt_dashes(int *ncol, int *ndash) {
@@ -66,16 +67,13 @@ void prit(SEXP df, int *init, int *num, int *ncol) {
 	}
 }
 
-int * find_max_per_col(SEXP df, int *inds, int nn, int ncol) {
-
-	// TODO reduce for loop repetition
+int * find_max_per_col(SEXP df, int *inds, int nn, R_xlen_t ncol) {
 
 	int l_inds = nn * 2;
 
 	int *maxs;
 	maxs = (int *) R_alloc(ncol, sizeof(int));
 
-	// rows:
 	for (R_xlen_t i = 0; i < ncol; i++) {
 
 		SEXP col = PROTECT(VECTOR_ELT(df, i));
@@ -91,7 +89,6 @@ int * find_max_per_col(SEXP df, int *inds, int nn, int ncol) {
 				int ki = inds[k] - 1;
 				sprintf(str, "%.1f", px[ki]);
 				ptr[k] = nchar(str);
-				Rprintf("i%i %s\n", i, str);
 			}
 		} else if (Rf_isInteger(col)) {
 			int *px;
@@ -100,14 +97,12 @@ int * find_max_per_col(SEXP df, int *inds, int nn, int ncol) {
 				int ki = inds[k] - 1;
 				sprintf(str, "%i", px[ki]);
 				ptr[k] = nchar(str);
-				Rprintf("i%i %s\n", i, str);
 			}
 		} else if (Rf_isString(col)) {
 			for (int k = 0; k < l_inds; k++) {
 				int ki = inds[k] - 1;
 				sprintf(str, "%s", CHAR(STRING_ELT(col, ki)));
 				ptr[k] = nchar(str);
-				Rprintf("i%i %s\n", i, str);
 			}
 		} else if (Rf_isFactor(col)) {
 			SEXP attr = PROTECT(Rf_asCharacterFactor(col));
@@ -115,44 +110,38 @@ int * find_max_per_col(SEXP df, int *inds, int nn, int ncol) {
 				int ki = inds[k] - 1;
 				sprintf(str, "%s", CHAR(STRING_ELT(attr, ki)));
 				ptr[k] = nchar(str);
-				Rprintf("i%i %s\n", i, str);
 			}
 			UNPROTECT(1);
 		} else if (Rf_isLogical(col)) {
 			int *px;
 			px = LOGICAL(col);
 			for (int k = 0; k < l_inds; k++) {
-				// TODO what about NA values?
+				// TODO error: what about NA values?
 				char *s = (px[k] == 1) ? "TRUE" : "FALSE";
 				ptr[k] = nchar(s);
-				Rprintf("i%i %s\n", i, s);
 			}
 		} else {
 			Rf_error("unknown column type");
 		}
 		int max_col = int_maxima(ptr, &l_inds);
-		Rprintf("%i max_col: %i\n", i, max_col);
 
 		/* column names: */
-		// SEXP colnames = PROTECT(Rf_getAttrib(df, R_NamesSymbol));
-		// const char * colname = CHAR(STRING_ELT(colnames, i));
-		// int l_colname = strlen(colname);
+		SEXP colnames = PROTECT(Rf_getAttrib(df, R_NamesSymbol));
+		const char * colname = CHAR(STRING_ELT(colnames, i));
+		int len_colname = strlen(colname);
 
-		// get the higher one for maximum:
-		// if (max_col > l_colname) {
-		// 	maxs_cols[i] = max_col;
-		// } else {
-		// 	maxs[i] = l_colname;
-		// }
-
-		// UNPROTECT(2);
+		/* higher length one becoming maximum for that col: */
+		if (max_col > len_colname) {
+			maxs[i] = max_col;
+		} else {
+			maxs[i] = len_colname;
+		}
+		UNPROTECT(2);
 	}
-
-	// random value for maxs for now:
-	*maxs = 101;
 	return maxs;
 }
 
+/* finds indices started on R index level */
 int * find_indices(int nn, int nrow) {
 	int dnn = nn * 2;
 	int *arr;
@@ -161,7 +150,7 @@ int * find_indices(int nn, int nrow) {
 	int i, j;
 	for(i = 0, j = remain; i < dnn; i++) {
 		if (i < nn) {
-			arr[i] = i+1;
+			arr[i] = i + 1;
 		} else {
 			arr[i] = j;
 			j = j + 1;
@@ -188,19 +177,17 @@ SEXP _ht (SEXP df, SEXP n) {
 	const R_xlen_t nrow = Rf_xlength(Rf_getAttrib(df, R_RowNamesSymbol));
 	const R_xlen_t ncol = Rf_xlength(df);
 
-	// start :
-	// int nrowlen = nint((int*)&nrow);
-
 	int *indices;
 	indices = find_indices(nn, nrow);
 
 	int *max_length;
-	max_length = find_max_per_col(df, indices, nn, (int)ncol);
+	max_length = find_max_per_col(df, indices, nn, ncol);
 
-	/* prt_colnames(df, &dim.ncoli, &dim.nrowlen);
+	prt_colnames(df, ncol, nrow);
 
+	/* head: */
 	int begin = 0;
-	prit(df, &begin, &dim.nn, &dim.ncoli);
+	prt_ht(df, indices, nn, ncol, max_length);
 
 	prt_hspace(&dim.nrowlen);
 
