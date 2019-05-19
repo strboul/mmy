5f98b184ee62400a6ad8267964741cd4cc54cbb1
Metin Yazici <stradivariusboul@gmail.com>
2019-05-19 12:50:20 +0200

    Change .Call interface

--- R/sln.R
+++ R/sln.R
@@ -4,11 +4,13 @@
 #' Useful when working with large lists. Especially for the content return as
 #' JSON type converted into R list type.
 #'
+#' Implemented with breadth-first search as it is more natural to see list elements.
+#'
 #' @param list list object.
 #' @param query keyword to look in the list names.
 #' @export
 search_list_names <- function(list, query) {
-  ret <- .Call("_sln", PACKAGE = "mmy", list, query)
-  invisible(ret)
+  invisible(.Call(`_sln`, list, query, PACKAGE = "mmy"))
 }
+
 
