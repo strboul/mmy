f14f3dce2b1a299239f0b4a5e4cedb997f898b83
Metin Yazici <stradivariusboul@gmail.com>
2019-05-26 22:22:28 +0200

    Add data and fix output

--- tests/sln-output.R
+++ tests/sln-output.R
@@ -1,13 +1,26 @@
 
+## Test output from command line
+## - assume that `pwd` is the same as the location of the `DESCRIPTION` file)
+## - command wrapped between parentheses as it gonna run in a subshell
+## (cd tests/ && Rscript --vanilla -e "source('sln-output.R')")
+##
+## Test output from R
+## setwd('tests');source('sln-output.R')
+## 
+
+source("test-helpers.R")
+
 ## a sample list object (countries):
 
-file <- file.path("tests", "data", "countries.json")
-txt <- paste(readLines(file), collapse = "")
+# countries <- readRDS(file.path("data", "countries.RDS"))
+basic <- readRDS(file.path("data", "basic.RDS"))
 
-## a list all elements are properly named:
-ALL.NAMED.LIST <- jsonlite::fromJSON(txt)
-
-mmy::search_list_names(ALL.NAMED.LIST, "name")
+# mmy::search_list_names(basic, "elephant")
+# mmy::search_list_names(basic, "color")
 
 ## a list which all elements are not properly named:
 # TODO
+
+# cat("==========================\n")
+mmy::search_list_names(basic, "name")
+
