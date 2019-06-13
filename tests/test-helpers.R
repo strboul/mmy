f86b5720c29efcddec0427082ef8f141b13959ed
Metin Yazici <stradivariusboul@gmail.com>
2019-06-13 20:40:22 +0200

    Extend test helpers

--- tests/test-helpers.R
+++ tests/test-helpers.R
@@ -1,12 +1,49 @@
 
+library(rdrtests)
+
+test_suite <- function(section, ...) {
+  if (!is.character(section) && length(section) == 1L) {
+    stop("section must be a character")
+  }
+  cat(sprintf("Testing `%s` ...", section), "\n")
+  dots <- list(...)
+  invisible(local(dots))
+  cat("success\n\n")
+}
+
 is_error <- function(call) {
   k <- tryCatch(call, error = function(e) e)
-  if(is.null(k$message)) {
-    stop(deparse(substitute(call)), " didn't throw an error.", call. = FALSE)
+  if (!"simpleError" %in% class(k)) {
+    msg <- sprintf("`%s` %s", deparse(substitute(call)), "did not throw any error.")
+    stop(msg, call. = FALSE)
+  }
+}
+
+is_identical <- function(actual, expected) {
+  res <- identical(actual, expected)
+  if (!res) {
+    msg <- sprintf("`%s` and `%s` are not identical", 
+                   deparse(substitute(actual)), 
+                   deparse(substitute(expected))
+                   )
+    stop(msg, call. = FALSE)
+  }
+}
+
+is_equal <- function(actual, expected) {
+  res <- all.equal(actual, expected)
+  if (!isTRUE(res)) {
+    msg <- sprintf("`%s` and `%s` are not equal\n%s", 
+                   deparse(substitute(actual)), 
+                   deparse(substitute(expected)),
+                   res)
+    stop(msg, call. = FALSE)
   }
 }
 
 #' Multiple expect
+#' 
+#' Do it for the calls including native code.
 #' 
 #' @param x.call a valid R call.
 #' @param n number of times.
