f76e491c5e8b31872aae8c6de8d79c8d348647e2
Metin Yazici <stradivariusboul@gmail.com>
2019-06-14 20:14:37 +0200

    Fix wrong travis user object name that should be `IS.TRAVIS`

--- tests/sln-error.R
+++ tests/sln-error.R
@@ -3,4 +3,4 @@ source("test-helpers.R")
 
 basic <- readRDS(file.path("data", "basic.RDS"))
 
-multiple_expect(mmy::search_list_names(basic, "color"), n = 10, use.gctorture = IS_TRAVIS)
+multiple_expect(mmy::search_list_names(basic, "color"), n = 10, use.gctorture = IS.TRAVIS)
