31ff71773ec70d4772d5b9bb9b07600c966b4f3f
Metin Yazici <stradivariusboul@gmail.com>
2019-06-22 12:56:32 +0200

    More `tlist()` work

--- R/tlist.R
+++ R/tlist.R
@@ -1,29 +1,48 @@
 
+### ----------------------------------------------------------------- ###
+### TLIST ----
+### ----------------------------------------------------------------- ###
+
 #' An opinionated way to construct (tree-like) lists
-#' 
-#' @details 
-#' Some specifications:
-#' - There're no list names in \code{tlist}, only indices.
-#' - The list is flat. The nodes staying at the same level are not wrapped in another
-#' list, rather a new node has been created.
-#' - 'tlist's are designed to be used in small, not long stuctures. The semantics
+#'
+#' (WORK IN PROGRESS)
+#'
+#' \itemize{
+#' \item There're no list names in \code{tlist}, only indices.
+#' \item The list is flat. The nodes staying at the same level are not wrapped in
+#' another list, rather a new node has been created.
+#' \item 'tlist's are designed to be used in small, not long stuctures. The semantics
 #' doesn't allow to go super deep down.
+#' }
+#'
+#' @usage
+#' \code{tlist()} \cr
+#' \Sexpr[strip.white=FALSE,results=rd]{mmy:::tlist_example_tree()}
+#'
+#' @details
+#' As seen in the graph above,
 #' 
+#' \itemize{
+#' \item The indexes surrounded by parentheses appearing in each line indicate
+#' \emph{levels}, e.g. \code{(1)}.
+#' \item The indexes placed in the bar appearing under the letters indicate
+#' \emph{nodes}, e.g. \code{[5]}.
+#' }
+#'
 #' @examples
-#' TODO
 #' ## Instantiate a tlist to keep fruit data:
 #' fruits <- tlist()
-#' fruits$append("apple", level = 1)
-#' fruits$main()
+#' fruits$append("apple", 1, 1)
 #' ## Append a child
-#' fruits$append("seed", level = c(1, 2))
-#' fruits$main()
+#' fruits$append("seed", 1, 3)
 #' ## Append siblings:
-#' fruits$append(c("orange", "watermelon"), level = 1)
-#' }
+#' fruits$append(c("orange", "watermelon"), 1)
+#' ## Get the main list:
+#' fruits$main()
+#' 
 #' @export
 tlist <- function() {
-  
+
   ## /_____________________________________________/
   ## FIELDS
   ## - main: main `tlist` to be returned.
@@ -32,10 +51,10 @@ tlist <- function() {
     env = NULL,
     main = NULL
   )
-  
+
   ## /_____________________________________________/
   ## INSTANTIATION
-  ## When the `tlist` object created, assign fields 
+  ## When the `tlist` object created, assign fields
   ## in the global env.
   ## /_____________________________________________/
   instantize <- function() {
@@ -46,14 +65,14 @@ tlist <- function() {
       env = self$env
     )
   }
-  
+
   if (is.null(self$env)) {
     instantize()
-  } 
-  
+  }
+
   ## /_____________________________________________/
   ## METHODS
-  ## As a convention, private methods should start 
+  ## As a convention, private methods should start
   ## with dots.
   ## - new: create a new `tlist` list.
   ## - main: return main `tlist` list.
@@ -61,34 +80,36 @@ tlist <- function() {
   getEnv <- function() {
     self$env
   }
-  
+
   getMainList <- function() {
     structure(self$main, class = "tlist_main")
   }
-  
+
   getMainListLength <- function() {
     length(self$main)
   }
-  
-  append <- function(x, ...) {
-    
+
+  append <- function(x, level, node) {
+
   }
   
+  access <- function(level, node)
+
   # appendSibling <- function(x, ...) {
   #   levels <- list(...)
   #   index <- length(self$main) + 1L
   #   self$main[[index]] <<- list(x)
   # }
-  # 
+  #
   # appendChild <- function(x, ...) {
   #   levels <- list(...)
-  #   
+  #
   #   index <- length(self$main[[level]]) + 1L
   #   self$main[[level]][[index]] <<- x
   # }
-  
+
   # TODO .makeIndex <- function(x)
-  
+
   ## /_____________________________________________/
   ## Exported objects.
   ## /_____________________________________________/
@@ -98,10 +119,10 @@ tlist <- function() {
       main = getMainList,
       mainListLength = getMainListLength,
       append = append
-    ), 
+    ),
     class = "tlist"
   )
-  
+
   exported
 }
 
@@ -111,6 +132,7 @@ tlist <- function() {
 
 #' Eliminate partial matching of dollar sign
 #' @export
+#' @noRd
 `$.tlist` <- function(x, name) {
   name <- as.character(name)
   res <- base::`[[`(x, name)
@@ -122,6 +144,7 @@ tlist <- function() {
 }
 
 #' @importFrom utils capture.output
+#' @noRd
 print.tlist <- function(x, ...) {
   env.name <- utils::capture.output(x$env())
   methods <- names(x)[sapply(x, typeof) == "closure"]
@@ -140,6 +163,7 @@ print.tlist <- function(x, ...) {
   )
 }
 
+#' @noRd
 print.tlist_main <- function(x, ...) {
   if (identical(length(x), 0L)) {
     cat("tlist()")
@@ -147,5 +171,34 @@ print.tlist_main <- function(x, ...) {
     cat("<tlist>\n\n")
     print(unclass(x))
   }
+}
+
+### ----------------------------------------------------------------- ###
+### UTILS ----
+### ----------------------------------------------------------------- ###
+
+#' Documentation macro used in the 'tlist' documentation.
+#' @noRd
+tlist_example_tree <- function() {
+  tree <- '
+  ________________________________________________________
+  (1)           A          B              C               
+  _____________[1]________[2]____________[3]______________
+              / | \        |        /   |   |    \        
+  (2)       A   B  C       A       A   B    C     D       
+  _____________[1]________[2]____________[3]______________
+           / \     |    / | | \        |    |   / | \     
+  (3)     A   B    A   A  B C  D       A    A  A  B  C    
+  _________[1]____[2]_____[3]_________[4]__[5]___[6]______
+  (...)
+  '
+  
+  ## Replace all new lines with '\cr'
+  tree <- gsub(paste0("\\", "n"), paste0("\\\\", "cr"), tree)
+  
+  ## Escape slashes:
+  #TODO
+  
+  tree
 }
 
