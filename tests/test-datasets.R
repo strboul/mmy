c28c76d9d314340e9b46189fe770a2e2352928f9
Metin Yazici <stradivariusboul@gmail.com>
2019-10-12 10:20:37 +0200

    Organize RDS files in `tests/data/` folder

--- tests/test-datasets.R
+++ tests/test-datasets.R
@@ -1,8 +1,7 @@
 
-## Test datasets
-## =============
+## Test datasets header
+## ====================
 
-# TODO wrap them in `normalizePath`?
 countries <- readRDS(file.path("data", "countries.RDS"))
 basic <- readRDS(file.path("data", "basic.RDS"))
 
