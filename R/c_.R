7db0eb475f4fb9bfaf44576686c52b9e3a7b5975
Metin Yazici <stradivariusboul@gmail.com>
2020-12-18 11:13:36 +0100

    Implement c_

---
+++ R/c_.R
@@ -0,0 +1,17 @@
+#' Override base R allowing to put commas at the end (trailing comma)
+#'
+#' @param ... objects to be passed onto [base::c].
+#' @references
+#' \url{https://gist.githubusercontent.com/flodel/5283216/raw/a781f7f320bd11e41a2c81affe5bec6f226dbf5e/ok.comma.R}
+#' @export
+c_ <- function(...) {
+  arg_objects <- as.list(match.call())[-1L]
+  len <- length(arg_objects)
+  if (len > 1L) {
+    last_obj <- arg_objects[[len]]
+    if (missing(last_obj)) {
+      arg_objects <- arg_objects[-len]
+    }
+  }
+  do.call(base::c, arg_objects)
+}
